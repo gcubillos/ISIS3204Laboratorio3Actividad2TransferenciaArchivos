4431af4fb5c3333c383338012f1913b50c51e642
Gabriel <g.cubillosb@uniandes.edu.co>
2020-04-01 07:58:17 -0500

    Agrega hashing de archivo y archivos transferencia
    Realiza puntos 4.2.2, 4.2.3, 4.2.4

--- ServidorUDP.py
+++ ServidorUDP.py
@@ -1,31 +1,86 @@
 # Código se basa en: https://pythontic.com/modules/socket/udp-client-server-example
 # https://www.binarytides.com/programming-udp-sockets-in-python/
+# Hashing: https://www.pythoncentral.io/hashing-files-with-python/
+# Transferencia: https://www.thepythoncode.com/article/send-receive-files-using-sockets-python
+# https://wiki.python.org/moin/UdpCommunication#Using_UDP_for_e.g._File_Transfers
+import math
+import os
 import socket
+import hashlib
+import tqdm
+from pip._vendor.distlib.compat import raw_input
 
-localIP = "127.0.0.1"
+# Punto 1
+ipLocal = "127.0.0.1"
 
-localPort = 20001
+puertoLocal = 20001
 
-bufferSize = 1024
+# Tamanio en el que se va a fragmentar los archivos.
+# Punto 2
+tamanioBuffer = raw_input('Defina el tamanio de los mensajes en que se van a fragmentar los archivos. Ingrese un numero '
+                          'entre 1 - 65536')
+tamanioBuffer = 1024
 
-msgFromServer = "Hello UDP Client"
+# Ubicar archivos
 
-bytesToSend = str.encode(msgFromServer)
+# Nombre archivo multimedia
+archivoMultimedia = "Archivos a Transferir/Multimedia.mov"
 
-# Create a datagram socket
+# Tamanio archivo multimedia
+tamanioMultimedia = os.path.getsize(archivoMultimedia)
 
-UDPServerSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+# Nombre archivo driver sonido
+archivoDriver = "Archivos a Transferir/224MB.exe"
+
+# Tamanio archivo
+tamanioDriver = os.path.getsize(archivoDriver)
+
+# Punto 3
+# Numero de fragmentos
+fragmentosMultimedia = math.ceil(tamanioMultimedia/tamanioBuffer)
+fragmentosDriver = math.ceil(tamanioDriver/tamanioBuffer)
+
+# Calcular Hash de archivo utilizando MD5
+# Multimedia
+hashMultimedia = hashlib.md5()
+with open('Archivos a Transferir/Multimedia.mov', 'rb') as aMultimedia:
+    buf = aMultimedia.read(tamanioBuffer)
+    while len(buf) > 0:
+        hashMultimedia.update(buf)
+        buf = aMultimedia.read(tamanioBuffer)
+valorHashMultimedia = hashMultimedia.hexdigest()
+
+# Driver
+hashDriver = hashlib.md5()
+with open('Archivos a Transferir/224MB.exe', 'rb') as aDriver:
+    buf = aDriver.read(tamanioBuffer)
+    while len(buf) > 0:
+        hashDriver.update(buf)
+        buf = aDriver.read(tamanioBuffer)
+valorHashDriver = hashDriver.hexdigest()
+
+
+# Crear socket
+
+socketServidorUDP = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
 
 # Bind to address and ip
 
-UDPServerSocket.bind((localIP, localPort))
+socketServidorUDP.bind((ipLocal, puertoLocal))
 
-print("UDP server up and listening")
+print("Servidor UDP abierto a conexiones")
 
-# Listen for incoming datagrams
+# Seleccionar archivo
+archivoSeleccionado = int(raw_input('Seleccione el archivo a enviar: 0: Multimedia (117 MB) 1: Driver (224 MB)'))
 
+# Numero de clientes a enviar archivo
+numClientes = int(raw_input('Indique el numero de clientes a los que se les va a enviar el archivo en simultaneo'))
+
+# Esperar mensajes de clientes
+numClientesConectados = 0
 while (True):
-    bytesAddressPair = UDPServerSocket.recvfrom(bufferSize)
+
+    bytesAddressPair = socketServidorUDP.recvfrom(tamanioBuffer)
 
     message = bytesAddressPair[0]
 
@@ -36,7 +91,3 @@ while (True):
 
     print(clientMsg)
     print(clientIP)
-
-    # Sending a reply to client
-
-    UDPServerSocket.sendto(bytesToSend, address)
